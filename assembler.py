271458b2f73f4a3e032d15d385fe0edbac3cba27
17navneet <navneetajmani17@gmail.com>
2025-02-18 18:13:47 +0530

    Update assembler.py

--- assembler.py
+++ assembler.py
@@ -1,1 +1,50 @@
-# wrtie your code here guys
+import sys
+
+r_type_instructions = {
+    "add": {"opcode": "0110011", "rd": "", "funct3": "000", "rs1": "", "rs2": "", "funct7": "0000000"},
+    "sub": {"opcode": "0110011", "rd": "", "funct3": "000", "rs1": "", "rs2": "", "funct7": "0100000"},
+    "slt": {"opcode": "0110011", "rd": "", "funct3": "010", "rs1": "", "rs2": "", "funct7": "0000000"},
+    "srl": {"opcode": "0110011", "rd": "", "funct3": "101", "rs1": "", "rs2": "", "funct7": "0000000"},
+    "or": {"opcode": "0110011", "rd": "", "funct3": "110", "rs1": "", "rs2": "", "funct7": "0000000"},
+    "and": {"opcode": "0110011", "rd": "", "funct3": "111", "rs1": "", "rs2": "", "funct7": "0000000"}
+}
+
+i_type_instructions = {
+    "lw": {"opcode": "0000011","rd": "", "funct3": "010", "rs1": "",  "imm": ""},
+    "addi": {"opcode": "0010011","rd": "", "funct3": "000", "rs1": "",  "imm": ""},
+    "jalr": {"opcode": "1100111","rd": "", "funct3": "000", "rs1": "",  "imm": ""}
+}
+
+s_type_instructions = {
+    "sw": {"opcode": "0100011","imm":"", "funct3": "010","rs1":"","rs2":""},
+}
+
+b_type_instructions = {
+    "beq": {"opcode": "1100011", "imm1": "", "funct3": "000", "rs1": "", "rs2": "", "imm2": ""},
+    "bne": {"opcode": "1100011", "imm1": "", "funct3": "001", "rs1": "", "rs2": "", "imm2": ""},
+    "blt": {"opcode": "1100011", "imm1": "", "funct3": "100", "rs1": "", "rs2": "", "imm2": ""},
+}
+
+j_type_instructions = {
+    "jal": {"opcode": "1101111","rd":"","imm":""},
+}
+
+def bin_str(num):                
+    return bin(num)[2:]   
+
+
+def Immediate(n,b):
+    if n >=0:
+        binary = bin(n)[2:]
+    else:
+        binary = bin(n & int("1" * (n.bit_length() + 1), 2))[2:]
+   
+    if len(binary) < b:
+        if n >= 0:
+            binary = '0' * (b - len(binary)) + binary
+        else:                                                                       
+            binary = '1' * (b - len(binary)) + binary
+    elif len(binary) > b:
+        binary = binary[-b:]
+        
+    return binary
